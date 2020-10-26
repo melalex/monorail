bb1eeb5b856e2db9470dd4915053838c6bd3c642
Alexander Melashchenko <melashhenko.o@ajax.systems>
2020-10-26 18:59:10 +0200

    feat: edit playbook

--- terraform/main.tf
+++ terraform/main.tf
@@ -56,6 +56,7 @@ resource "google_compute_instance" "this" {
     ]
 
     connection {
+      host = google_compute_instance.this.network_interface.0.access_config.0.nat_ip
       type = "ssh"
       user = var.compute_instance_username
       private_key = local_file.private_key.filename
